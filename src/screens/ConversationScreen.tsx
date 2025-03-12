78d5619d448d9b2c303887766ecb7d7af9e8c49f
Purnadi <purnadi.winarno@automate-app.com>
2025-03-12 14:48:10 +0700

    install react navigation, reanimated, navigation structure

---
+++ src/screens/ConversationScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function ConversationScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Conversation Screen</Text>
+    </View>
+  );
+}
+
+export default ConversationScreen; 
